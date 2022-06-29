9bba1fa74318505c4c9863e8bec65e423a1db0e6
pg22nicholas <pg22nicholas@vfs.com>
2022-06-29 11:17:35 -0700

    Added docs public github page

---
+++ docs/precache-manifest.15756135d541c419f7e6675648ac474b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6c2b1b118d27d41b15",
+    "url": "/css/app.3e6d8e7d.css"
+  },
+  {
+    "revision": "3a79cf19f40eb8f6b274054b02305aec",
+    "url": "/css/media.css"
+  },
+  {
+    "revision": "e390ea4e07fbe17c5d271e38fbb8ba56",
+    "url": "/css/style-base.css"
+  },
+  {
+    "revision": "79ac9743b6fff790b901b3e12acc7b66",
+    "url": "/css/style-grid.css"
+  },
+  {
+    "revision": "64537337291c7503d5cf75abf4474a71",
+    "url": "/css/style-menu.css"
+  },
+  {
+    "revision": "9a2be61d02dbd882c8a92b6fa60d5e91",
+    "url": "/img/vfs_logo.9a2be61d.png"
+  },
+  {
+    "revision": "b85fd1906edf1eefb85ed5639d15a4bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6c2b1b118d27d41b15",
+    "url": "/js/app.f75b80fe.js"
+  },
+  {
+    "revision": "2cb3e6bbbf7515f7cc0f",
+    "url": "/js/chunk-vendors.fe571987.js"
+  },
+  {
+    "revision": "b2faedf5784d9948a56a6329179d647f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
