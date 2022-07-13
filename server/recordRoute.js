bb850ebdbe91042b036ef908a91efd566f8bb881
pg22nicholas <pg22nicholas@vfs.com>
2022-07-13 01:02:06 -0700

    Added endpoint to delete record by id

--- server/recordRoute.js
+++ server/recordRoute.js
@@ -10,30 +10,41 @@ const Router = Express.Router();
 
 Router.delete('/record', ( request, response, next ) => {
     
+    /*
+    {id }
+    */
     const params = { ...request.params, ... request.query, ...request.body };
 
-    response.send("something");
+    let id = params.id
+
+    // if id doesn't exist, failure
+    if (!recordList[id]) {
+        return response.status(101).end()
+    }
+    delete recordList[id]
+    console.log(recordList)
+    response.send("Successfully delete id: " + id);
     next();
 })
 
 Router.post('/record', ( request, response, next ) => {
 
     /*
-    { record: { version: "2022.07.06-1234",                 // (string from data matching game)-session
-                sessionId: 1234,                            // session within version
-                eventId: 1,                                 // Event triggered by telemetry data (ENUM)
-                location: { X: 0, Y: 0 },                   // location at event call   
-                mapName: "base_map",                        // Name of map
+    { record: { version: String            
+                sessionId: Int
+                eventId: EventTriggerEnum
+                location: { X: Int, Y: Int },  
+                mapName: String
                 actor: {
-                    id: 0,                                  // actor id within session
-                    state: 0,                               // Animation state (ENUM)
-                    health: 100,                            // Current health
-                    damageDone: 0,                          // Damage done over actor's lifetime
-                    weapon: 2,                              // Current weapon equipped
-                    heading: { X: 0, Y: 0, Z: 0 },          // Vector where actor is going
-                    lookingVector: { X: 0, Y: 0, Z: 0 },    // Direction actor is looking (Normalized)
-                    spawnAt: { X: 0, Y: 0, Z: 0 },          // Actor initial spawn location
-                    travelled: 100,                         // Total distance actor travelled since spawn
+                    id: Int
+                    state: AnimationStateEnum
+                    health: Int
+                    damageDone: Int
+                    weapon: Int
+                    heading: { X: Int, Y: Int, Z: Int },        
+                    lookingVector: { X: Int, Y: Int, Z: Int },    
+                    spawnAt: { X: Int, Y: Int, Z: Int },          
+                    travelled: Int
                 }   
             }
     }
