b54ad98619a1e1881ea8e8f4da46a1324b1d6b52
pg22nicholas <97759807+pg22nicholas@users.noreply.github.com>
2022-08-27 01:29:10 -0700

    updated route data format for data from the client

--- server/api/FirebaseRoute.js
+++ server/api/FirebaseRoute.js
@@ -2,52 +2,59 @@
 const Express = require('express')
 const Result = require('../../src/mixins/result.js')
 
-import FirebaseConnection from '../../src/store/FirebaseConnection.js';
+import FirebaseConnection from '../../src/store/FirebaseConnection.js'
 const db = new FirebaseConnection()
 
-const Router = Express.Router();
+const Router = Express.Router()
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 // retrieve a new record and send to Firebase to store
 Router.post('/record_list', ( request, response, next ) => {
     
     /*
-    [
-        {
-            objectType: Int, (ex. Player, Enemy, Damage, ...)
-            sessionID: String (year.month.day:sessionID)
-            data { 
-                ...
-            }
-        },
-    ...,
-    ]      
+    {
+        data: 
+        [
+            {
+            objectType: Int, (ex. Player, Enemy, Damage, ...),
+            sessionID: String (year.month.day:sessionID),
+            ...
+            },
+            ...
+        ]
+    } 
     */
-    const params = { ...request.params, ... request.query, ...request.body };
+    const params = { ...request.body }
+    const data = params.data
+
+    for (let record in data) {
+        // TODO: Send data to firebase
+        console.log(data[record].objectType, data[record].sessionID)
+        //db.add(`/api/tdata/record/${record.objectType}`, record)
+    }
     
-    // TODO: Send data to firebase
-    console.log(params[0].objectType, params[0].sessionID)
-    next();
+    response.send()
+    next()
 })
 
 // Generate a new unique session ID
 Router.get('/generate-session-id', ( request, response, next ) => {
     
-    let date_time = new Date();
+    let date_time = new Date()
 
     // get current date
     // adjust 0 before single digit date
-    let day = ("0" + date_time.getDate()).slice(-2);
+    let day = ("0" + date_time.getDate()).slice(-2)
 
     // get current month
-    let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+    let month = ("0" + (date_time.getMonth() + 1)).slice(-2)
 
     // get current year
-    let year = date_time.getFullYear();
+    let year = date_time.getFullYear()
 
-    response.send(`${year}-${month}-${day}:${uuidv4()}`);
-    next();
+    response.send(`${year}-${month}-${day}:${uuidv4()}`)
+    next()
 })
 
 
